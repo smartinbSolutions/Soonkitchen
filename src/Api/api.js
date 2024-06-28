9d825a007c926f78dbefe5547254f6c589a6d3dc
Abdulrahman Hasanato <byjyta@gmail.com>
2024-06-28 13:53:58 +0300

    updated api

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,4 +1,5 @@
-export const baseURL = "ec2-51-20-55-62.eu-north-1.compute.amazonaws.com:8001";
+export const baseURL =
+  "http://ec2-51-20-55-62.eu-north-1.compute.amazonaws.com:8001";
 export const BrandsEndPoint = "/api/soonkitchen";
 export const LabelsEndPoint = "/api/soonkitchenlabel";
 export const RecipesEndPoint = "/api/soonkitchenrecipe";
