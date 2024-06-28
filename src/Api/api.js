d3588ae9cfea004d1e0dd3cc7aa3f45633803cb3
Abdulrahman Hasanato <51422521+hasanato01@users.noreply.github.com>
2024-06-28 13:45:33 +0300

    Update api.js
    I hope it's the last time

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,5 +1,4 @@
-export const baseURL =
-  "https://ec2-51-20-253-41.eu-north-1.compute.amazonaws.com:8001";
+export const baseURL = "ec2-51-20-55-62.eu-north-1.compute.amazonaws.com:8001";
 export const BrandsEndPoint = "/api/soonkitchen";
 export const LabelsEndPoint = "/api/soonkitchenlabel";
 export const RecipesEndPoint = "/api/soonkitchenrecipe";
