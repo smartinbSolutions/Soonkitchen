d6d32a2f3c3441d903998f1abc45df7a20adfd63
Abdulrahman Hasanato <byjyta@gmail.com>
2024-06-28 11:46:08 +0300

    updated build

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,4 +1,5 @@
-export const baseURL = "https://nooncar.com:8001";
+export const baseURL =
+  "https://ec2-51-20-253-41.eu-north-1.compute.amazonaws.com:8001";
 export const BrandsEndPoint = "/api/soonkitchen";
 export const LabelsEndPoint = "/api/soonkitchenlabel";
 export const RecipesEndPoint = "/api/soonkitchenrecipe";
